26fa18659f95bafd87ea99203858dbf75bf29a43
Kimitoshi Takahshi <ktaka@ccmp.jp>
2018-11-21 22:50:00 +0900

    wip

---
+++ convert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=./Figs
+shopt -s nullglob
+
+for f in $DIR/*.svg ; do
+echo cairosvg  -s 10 $f -o ${f%.*}.png
+cairosvg  -s 10 $f -o ${f%.*}.png
+done
+
+for f in $DIR/*.png ; do
+echo mogrify -trim $f 
+mogrify -trim $f 
+done
+
+shopt -u nullglob
+
