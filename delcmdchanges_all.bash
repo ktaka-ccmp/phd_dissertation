efdab4916c899532772d657ec2f47dd77e77116e
Kimitoshi Takahashi <ktaka@ccmp.jp>
2019-09-13 09:13:11 +0900

    strip changes

--- delcmdchanges_all.bash
+++ delcmdchanges_all.bash
@@ -4,6 +4,8 @@ FROM=Manuscript_changes
 TO=Manuscript_stripped
 FILES=$(egrep input  ${FROM}/body.tex |sed -e 's/}/\.tex/g'|cut -f 2 -d "/")
 
+mkdir -p ${TO}
+
 cp ${FROM}/body.tex ${TO}/body.tex
 
 for file in ${FILES} abstract.tex acknowledgment.tex appendix.tex ; do 
